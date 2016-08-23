1d35accb1b6f3e36127e54a857c3c0cc411ec29d
xugang <xugang@xugangs-MacBook-Pro.local>
2016-08-23 23:13:47 +0800

    创建用户详情,登录时纪录ip

--- user-center-biz/src/main/java/com/pop/uc/dao/LoginLogDao.java
+++ user-center-biz/src/main/java/com/pop/uc/dao/LoginLogDao.java
@@ -1,11 +1,12 @@
 package com.pop.uc.dao;
 
 import com.pop.uc.entity.LoginLogEntity;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by xugang on 16/8/9.
  */
-
+@Repository
 public interface LoginLogDao {
     public void save(LoginLogEntity loginLogEntity);
 
